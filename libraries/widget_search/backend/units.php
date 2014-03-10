76e7e4edc9e14c2542b07c364ec53dbfc24df45f
realtyna <realtyna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-10 10:49:55 +0000

    [WPL] Version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/real-estate-listing-realtyna-wpl/tags/1.0.0@872660 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ libraries/widget_search/backend/units.php
@@ -0,0 +1,56 @@
+<?php
+/** no direct access * */
+defined('_WPLEXEC') or die('Restricted access');
+
+if(($type == 'area' or $type == 'price' or $type == 'length' or $type == 'volume') and !$done_this)
+{
+    ?>
+
+    <div class="search-field-wp search-field-units <?php echo $value['enable']; ?>"
+         data-field-id="<?php echo $field->id; ?>"
+         data-status="<?php echo $value['enable']; ?>"
+         data-field-name="<?php echo __($field->name, WPL_TEXTDOMAIN); ?>"
+         data-field-order="<?php echo $value['sort']; ?>">
+
+        <input type="hidden" id="field_sort_<?php echo $field->id; ?>"
+               name="<?php echo $this->get_field_name('data'); ?>[<?php echo $field->id; ?>][sort]"
+               value="<?php echo $value['sort']; ?>"/>
+        <input type="hidden" id="field_enable_<?php echo $field->id; ?>" onchange="elementChanged(true);"
+               name="<?php echo $this->get_field_name('data'); ?>[<?php echo $field->id; ?>][enable]"
+               value="<?php echo $value['enable']; ?>"/>
+        <input type="hidden" id="field_id_<?php echo $field->id; ?>"
+               name="<?php echo $this->get_field_name('data'); ?>[<?php echo $field->id; ?>][id]"
+               value="<?php echo $value['id']; ?>"/>
+
+        <h4>
+            <span>
+                <?php echo __($field->name, WPL_TEXTDOMAIN); ?>
+            </span>
+        </h4>
+
+        <div class="field-body">
+            <div class="erow">
+                <select name="<?php echo $this->get_field_name('data'); ?>[<?php echo $field->id; ?>][type]">
+                    <option
+                        value="minmax" <?php if($value['type'] == 'minmax') echo 'selected="selected"' ?>><?php echo __('Min/Max textbox', WPL_TEXTDOMAIN); ?></option>
+                    <option
+                        value="minmax_slider" <?php if($value['type'] == 'minmax_slider') echo 'selected="selected"' ?>><?php echo __('Min/Max Slider', WPL_TEXTDOMAIN); ?></option>
+                    <option
+                        value="minmax_selectbox" <?php if($value['type'] == 'minmax_selectbox') echo 'selected="selected"' ?>><?php echo __('Min/Max SelectBox', WPL_TEXTDOMAIN); ?></option>
+                    <option
+                        value="minmax_selectbox_plus" <?php if($value['type'] == 'minmax_selectbox_plus') echo 'selected="selected"' ?>><?php echo __('Min/Max SelectBox+', WPL_TEXTDOMAIN); ?></option>
+                </select>
+            </div>
+            <div class="erow wpl_extoptions_span">
+                <input type="text"
+                       name="<?php echo $this->get_field_name('data'); ?>[<?php echo $field->id; ?>][extoption]"
+                       value="<?php echo $value['extoption']; ?>"
+                       placeholder="<?php _e('min,max,increment like 0,10,1', WPL_TEXTDOMAIN); ?>"
+                       title="<?php _e('min,max,increment like 0,10,1', WPL_TEXTDOMAIN); ?>" />
+            </div>
+        </div>
+    </div>
+
+    <?php
+    $done_this = true;
+}
---
+++ widgets/search/scripts/js.php
@@ -0,0 +1,160 @@
+<script type="text/javascript">
+wplj(document).ready(function()
+{
+	wplj(".MD_SEP > .wpl_search_field_container:first-child").click(function()
+	{
+		wplj(this).siblings(".wpl_search_field_container").slideToggle(400)
+	})
+});
+
+/** main search function **/
+function wpl_do_search_<?php echo $widget_id; ?>()
+{
+	request_str = '';
+	wplj("#wpl_searchwidget_<?php echo $widget_id; ?> input:checkbox").each(function(index, element)
+	{
+		id = element.id;
+		name = element.name;
+		if(name.substring(0, 2) == 'sf')
+		{
+			if(wplj("#wpl_searchwidget_<?php echo $widget_id; ?> #"+id).closest('li').css('display') != 'none')
+			{
+				if(element.checked) value = element.value; else value = "-1";
+				request_str += "&" + element.name.replace('sf<?php echo $widget_id; ?>_', 'sf_') +"=" + value;
+			}
+		}
+	});
+
+	wplj("#wpl_searchwidget_<?php echo $widget_id; ?> input:text").each(function(index, element)
+	{
+		id = element.id;
+		name = element.name;
+		if(name.substring(0, 2) == 'sf')
+		{
+			if(wplj("#wpl_searchwidget_<?php echo $widget_id; ?> #"+id).closest('li').css('display') != 'none')
+			{
+				value = element.value;
+				request_str += "&" + element.name.replace('sf<?php echo $widget_id; ?>_', 'sf_') +"=" + value;
+			}
+		}
+	});
+
+	wplj("#wpl_searchwidget_<?php echo $widget_id;?> input[type=hidden]").each(function(index, element)
+	{
+		id = element.id;
+		name = element.name;
+		if(name.substring(0, 2) == 'sf')
+		{
+			if(wplj("#wpl_searchwidget_<?php echo $widget_id; ?> #"+id).closest('li').css('display') != 'none')
+			{
+				value = element.value;
+				request_str += "&" + element.name.replace('sf<?php echo $widget_id; ?>_', 'sf_') +"=" + value;
+			}
+		}
+	});
+	
+	wplj("#wpl_searchwidget_<?php echo $widget_id; ?> select, #wpl_searchwidget_<?php echo $widget_id; ?> textarea").each(function(index, element)
+	{
+		id = element.id;
+		name = element.name;
+		if(name.substring(0, 2) == 'sf')
+		{
+			if(wplj(element).closest('li').css('display') != 'none')
+			{
+				value = wplj(element).val();
+				if(value != null) request_str += "&" + element.name.replace('sf<?php echo $widget_id; ?>_', 'sf_') +"=" + value;
+			}
+		}
+	});
+	
+	/** Adding widget id **/
+	request_str = '?widget_id=<?php echo $widget_id; ?>'+request_str;
+
+	/** Create SEF url **/
+	request_str = wpl_sef_request<?php echo $widget_id; ?>(request_str);
+
+	/** Create full url of search **/
+	search_str = '<?php echo wpl_property::get_property_listing_link(); ?>'+request_str;
+	
+	window.location = search_str;
+	return false;
+}
+
+function wpl_sef_request<?php echo $widget_id; ?>(request_str)
+{
+	request_str = request_str.slice(1);
+	splited = request_str.split("&");
+	sef_str = '';
+	unsef_str = '';
+	var first_param = true;
+	
+	for(var i = 0; i < splited.length; i++)
+	{
+		splited2 = splited[i].split("=");
+		key = splited2[0];
+		value = splited2[1];
+		
+		if(key.substring(0, 9) == 'sf_select')
+		{
+			table_field = splited2[0].replace('sf_select_', '');
+			key = wpl_ucfirst(table_field.replace('_', ' '));
+			value = splited2[1];
+			
+			/** for setting text instead of value **/
+			if(value != -1 && value != '' && (table_field == 'listing' || table_field == 'property_type'))
+			{
+				field_type = wplj("#sf<?php echo $widget_id; ?>_select_"+table_field).prop('tagName');
+				if(field_type.toLowerCase() == 'select') value = wplj("#sf<?php echo $widget_id; ?>_select_"+table_field+" option:selected").text();
+			}
+			
+			/** set to the SEF url **/
+			if(value != -1 && value != '') sef_str += '/'+key+':'+value;
+		}
+		else
+		{
+			if(first_param && value != -1 && value != '')
+			{
+				unsef_str += '?'+key+'='+value;
+				first_param = false;
+			}
+			else if(value != -1 && value != '')
+			{
+				unsef_str += '&'+key+'='+value;
+			}
+		}
+	}
+	
+	final_str = sef_str+"/"+unsef_str;
+	return final_str.slice(1);
+}
+
+function wpl_add_to_multiple<?php echo $widget_id; ?>(value, checked, table_column)
+{
+	setTimeout("wpl_add_to_multiple<?php echo $widget_id; ?>_do('"+value+"', "+checked+", '"+table_column+"');", 30);
+}
+
+function wpl_add_to_multiple<?php echo $widget_id; ?>_do(value, checked, table_column)
+{
+	var values = wplj('#sf<?php echo $widget_id; ?>_multiple_'+table_column).val();
+	values = values.replace(value+',', '');
+	
+	if(checked) values += value+',';
+	wplj('#sf<?php echo $widget_id; ?>_multiple_'+table_column).val(values);
+}
+
+function wpl_select_radio<?php echo $widget_id; ?>(value, checked, table_column)
+{
+	console.log(value+":"+checked+":"+table_column);
+	if(checked) wplj('#sf<?php echo $widget_id;?>_select_'+table_column).val(value);
+}
+
+<?php
+	$this->create_listing_specific_js();
+	$this->create_property_type_specific_js();
+?>
+wplj(document).ready(function(){
+	wplj("select").chosen();
+    wplj('input[type="checkbox"]:not(.yesno)').checkbox({cls: 'jquery-safari-checkbox',empty:'<?php echo plugins_url();?>/WPL/assets/img/empty.png'});
+    wplj('input.yesno[type="checkbox"]').checkbox({empty:'<?php echo plugins_url();?>/WPL/assets/img/empty.png'});
+})
+</script>
