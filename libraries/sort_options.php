ef6721aade3bbc7510ed84983d4ac286e2b6c25f
realtyna <realtyna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-12 06:31:18 +0000

    Releasing WPL 1.0.1 search widget lightbox issue fixed location settings lightbox issue fixed
    git-svn-id: https://plugins.svn.wordpress.org/real-estate-listing-realtyna-wpl/tags/1.0.1@873848 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ libraries/sort_options.php
@@ -0,0 +1,58 @@
+<?php
+/** no direct access **/
+defined('_WPLEXEC') or die('Restricted access');
+
+/**
+** sort options Library
+** Developed 08/11/2013
+**/
+
+class wpl_sort_options
+{
+	/**
+		return sort options
+	**/
+	public function get_sort_options($kind = '', $enabled = 1, $condition = '', $output_type = 'loadAssocList')
+	{
+		if(trim($condition) == '')
+		{
+			$condition = "";
+			
+			if(trim($enabled) != '') $condition .= " AND `enabled`>='$enabled'";
+			if(trim($kind) != '') $condition .= " AND `kind` LIKE '%[$kind]%'";
+			$condition .= " ORDER BY `index` ASC";
+		}
+		
+		$query = "SELECT * FROM `#__wpl_sort_options` WHERE 1 ".$condition;
+		return wpl_db::select($query, $output_type);
+	}
+	
+	/**
+		@input $sort_ids
+	**/
+	public function sort_options($sort_ids)
+	{
+		$query = "SELECT `id`,`index` FROM `#__wpl_sort_options` WHERE `id` IN ($sort_ids) ORDER BY `index` ASC";
+		$options = wpl_db::select($query, 'loadAssocList');
+		
+		$conter = 0;
+		$ex_sort_ids = explode(',', $sort_ids);
+		
+		foreach($ex_sort_ids as $ex_sort_id)
+		{
+			self::update('wpl_sort_options', $ex_sort_id, 'index', $options[$conter]["index"]);
+			$conter++;
+		}
+	}
+	
+	/**
+		@input {table}, {key}, {id} and [value]
+		@return boolean result
+	**/
+	public function update($table = 'wpl_sort_options', $id, $key, $value = '')
+	{
+		/** first validation **/
+		if(trim($table) == '' or trim($id) == '' or trim($key) == '') return false;
+		return wpl_db::set($table, $id, $key, $value);
+	}
+}
---
+++ widgets/carousel/tmpl/default.php
@@ -0,0 +1,30 @@
+<?php
+defined('_WPLEXEC') or die('Restricted access');
+
+include _wpl_import("widgets.carousel.scripts.js", true, true);
+?>
+<div class="wpl_carousel_container">
+	<ul class="bxslider">
+		<?php 
+		foreach ($wpl_properties as $key => $gallery) {
+
+			if($gallery["items"]["gallery"][0]){
+				
+				$params = array();
+		        $params['image_name'] 		= $gallery["items"]["gallery"][0]->item_name;
+		        $params['image_parentid'] 	= $gallery["items"]["gallery"][0]->parent_id;
+		        $params['image_parentkind'] = $gallery["items"]["gallery"][0]->parent_kind;
+		        $params['image_source'] 	= wpl_global::get_upload_base_path().$params['image_parentid'].DS.$params['image_name'];
+
+		        if($gallery['raw']['property_title']) $image_title = $gallery['raw']['property_title'];
+		        else $image_title = $gallery['rendered'][3]['value'] .' '.$gallery['rendered'][2]['value'];
+				
+				$image_description	= $gallery["items"]["gallery"][0]->item_extra2;
+		        $image_url 			= wpl_images::create_gallary_image(1920, 558, $params, $watermark, $rewrite);
+
+				echo '<li><a href="'.$gallery["property_link"].'"><img src="'.$image_url.'" title="'.$image_title.'" alt="'.$image_title.'" /></a></li>';
+			}
+		}
+		?>
+	</ul>
+</div>
