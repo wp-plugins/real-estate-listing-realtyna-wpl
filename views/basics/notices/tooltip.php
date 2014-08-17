2c3dfcbb4e74de419895fddcaf67ba92b5515485
realtyna <realtyna@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-17 03:53:53 +0000

    Removed unused files
    git-svn-id: https://plugins.svn.wordpress.org/real-estate-listing-realtyna-wpl/tags/1.7.3@967196 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/basics/notices/tooltip.php
+++
@@ -1,6 +0,0 @@
-<?php
-/** no direct access **/
-defined('_WPLEXEC') or die('Restricted access');
-
-$rendered .= '<span class="wpl_help" id="wpl_notice'.$notice['id'].'"><span class="wpl_help_description">'.$notice['body'].'</span></span>';
-?>
